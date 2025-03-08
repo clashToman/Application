84eb9ccbf2cad02caebd98e64dbaed51e858bb1c
Cf224 <chinnafarming@gmail.com>
2025-03-08 21:35:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routes.auth import router
+from fastapi.middleware.cors import CORSMiddleware
 from routes.user import  user_router as user
 from routes.product import product_router as product
 from routes.order import order_router as orders
@@ -9,6 +10,14 @@ from routes.home import home_router as home
 from routes.subscription import subscription_router as subscription
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Frontend origin
+    allow_credentials=True,                   # Allow cookies/auth if needed
+    allow_methods=["GET", "POST", "OPTIONS"], # Allowed HTTP methods
+    allow_headers=["Content-Type"],           # Allowed headers (adjust if needed)
+)
 
 app.mount("/upload_image", StaticFiles(directory="uploads_image"), name="upload_image")
 
